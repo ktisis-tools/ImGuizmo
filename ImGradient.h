5b79e58bd13c8b88c73e0aa3dacd95efad6f1dcc
Fayti1703 <fayti1703@protonmail.com>
2023-06-01 19:24:03 +0200

    Convert indentation to tab
    I don't get space indentation. I really don't.
    
    Just set your tab size to your preferences,
    let me set my tab size to my preferences,
    indent with one tab, align with space (or don't align at all);
    and we're both happy.
    
    Filter used: `unexpand -t 3`

--- ImGradient.h
+++ ImGradient.h
@@ -31,14 +31,14 @@ struct ImVec2;
 
 namespace ImGradient
 {
-   struct Delegate
-   {
-      virtual size_t GetPointCount() = 0;
-      virtual ImVec4* GetPoints() = 0;
-      virtual int EditPoint(int pointIndex, ImVec4 value) = 0;
-      virtual ImVec4 GetPoint(float t) = 0;
-      virtual void AddPoint(ImVec4 value) = 0;
-   };
+	struct Delegate
+	{
+		virtual size_t GetPointCount() = 0;
+		virtual ImVec4* GetPoints() = 0;
+		virtual int EditPoint(int pointIndex, ImVec4 value) = 0;
+		virtual ImVec4 GetPoint(float t) = 0;
+		virtual void AddPoint(ImVec4 value) = 0;
+	};
 
-   bool Edit(Delegate& delegate, const ImVec2& size, int& selection);
+	bool Edit(Delegate& delegate, const ImVec2& size, int& selection);
 }
